a90ee7e7dde6552043a1a0cdfcee6d20763b0464
jimmy <ernestlogistics@gmail.com>
2014-06-11 10:36:40 +0800

    updated the addressBean

--- freightbit/src/main/java/com/sr/apps/freightbit/common/formbean/AddressBean.java
+++ freightbit/src/main/java/com/sr/apps/freightbit/common/formbean/AddressBean.java
@@ -1,23 +1,169 @@
 package com.sr.apps.freightbit.common.formbean;
 
+import java.util.Date;
+
 public class AddressBean {
 
-	private String address;
+
+    private String address;
 	private Integer addressId;
+    private Integer clientId;
+    private String referenceTable;
+    private Integer referenceId;
+    private String addressType;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private Integer zip;
+    private Date createdTimeStamp;
+    private String createdBy;
+    private Date modifiedTimeStamp;
+    private String modifiedBy;
 
-	public Integer getAddressId() {
-		return addressId;
-	}
 
-	public void setAddressId(Integer addressId) {
-		this.addressId = addressId;
-	}
+    public AddressBean(){}
 
-	public String getAddress() {
-		return address;
-	}
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    public AddressBean(Integer addressId, Integer clientId, String referenceTable, Integer referenceId,
+                       String addressType, String addressLine1, String addressLine2, String city,
+                       String state, Integer zip, Date createdTimeStamp, String createdBy, Date modifiedTimeStamp, String modifiedBy){
+
+            this.addressId = addressId;
+            this.clientId = clientId;
+            this.referenceTable = referenceTable;
+            this.referenceId = referenceId;
+            this.addressType = addressType;
+            this.addressLine1 = addressLine1;
+            this.addressLine2 = addressLine2;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.createdTimeStamp = createdTimeStamp;
+            this.createdBy = createdBy;
+            this.modifiedTimeStamp = modifiedTimeStamp;
+            this.modifiedBy = modifiedBy;
+
+
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Integer addressId) {
+        this.addressId = addressId;
+    }
+
+    public Integer getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Integer clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getReferenceTable() {
+        return referenceTable;
+    }
+
+    public void setReferenceTable(String referenceTable) {
+        this.referenceTable = referenceTable;
+    }
+
+    public Integer getReferenceId() {
+        return referenceId;
+    }
+
+    public void setReferenceId(Integer referenceId) {
+        this.referenceId = referenceId;
+    }
+
+    public String getAddressType() {
+        return addressType;
+    }
+
+    public void setAddressType(String addressType) {
+        this.addressType = addressType;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Integer getZip() {
+        return zip;
+    }
+
+    public void setZip(Integer zip) {
+        this.zip = zip;
+    }
+
+    public Date getCreatedTimeStamp() {
+        return createdTimeStamp;
+    }
+
+    public void setCreatedTimeStamp(Date createdTimeStamp) {
+        this.createdTimeStamp = createdTimeStamp;
+    }
+
+    public Date getModifiedTimeStamp() {
+        return modifiedTimeStamp;
+    }
+
+    public void setModifiedTimeStamp(Date modifiedTimeStamp) {
+        this.modifiedTimeStamp = modifiedTimeStamp;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
 }
