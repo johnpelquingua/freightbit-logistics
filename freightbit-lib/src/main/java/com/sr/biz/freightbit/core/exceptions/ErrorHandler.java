1e13973905ec586f4afb30ed921ee0c8e7834af0
Johnpel Quingua <johnpelquingua@gmail.com>
2014-12-16 17:55:14 -0800

    Added mvc dispatcher servlet for permission denial

---
+++ freightbit-lib/src/main/java/com/sr/biz/freightbit/core/exceptions/ErrorHandler.java
@@ -0,0 +1,39 @@
+package com.sr.biz.freightbit.core.exceptions;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ErrorHandler implements AccessDeniedHandler {
+
+    private String errorPage;
+
+    public ErrorHandler() {
+    }
+
+    public ErrorHandler(String errorPage) {
+        this.errorPage = errorPage;
+    }
+
+    public String getErrorPage() {
+        return errorPage;
+    }
+
+    public void setErrorPage(String errorPage) {
+        this.errorPage = errorPage;
+    }
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        //some business logic
+        response.sendRedirect(errorPage);
+
+    }
+
+}
