c0efc3b38af764fcbecd16718904d7d2713ca668
Johnpel Quingua <johnpelquingua@gmail.com>
2015-02-03 16:54:52 +0800

    Temp fix for live site (delete if necessary)

--- freightbit/src/main/java/com/sr/apps/freightbit/order/action/OrderAction.java
+++ freightbit/src/main/java/com/sr/apps/freightbit/order/action/OrderAction.java
@@ -261,6 +261,7 @@ public class OrderAction extends ActionSupport implements Preparable {
         System.out.println("The number of  new booking is " + Booking);
 
         return SUCCESS;
+
     }
 
     public String viewOrdersBookingList() {
@@ -1200,10 +1201,10 @@ public class OrderAction extends ActionSupport implements Preparable {
         }
         // for consignee contact person
         orderBean.setConsigneeContactPersonId(order.getConsigneeContactPersonId());
-        if (order.getConsigneeContactPersonId() != null) {
+        /*if (order.getConsigneeContactPersonId() != null) {
             Contacts contactElem = customerService.findContactById(order.getConsigneeContactPersonId());
             orderBean.setConsigneeContactName(contactElem.getFirstName() + " " + contactElem.getMiddleName() + " " + contactElem.getLastName());
-        }
+        }*/
 
         /*OUTBOUND DOCUMENTS*/
         /*outboundEntityList = documentsService.findDocumentByOutboundStageAndID(1, order.getOrderId());*/
